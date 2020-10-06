2ab6b4d742fbcb0ef00d4b96a25686ec09c40a10
hernandez87v <hernandez87v@hotmail.com>
2020-10-05 21:24:39 -0700

    added different R3F scene

--- src/index.js
+++ src/index.js
@@ -1,32 +1,55 @@
-import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import { Canvas, Dom } from 'react-three-fiber';
-import * as serviceWorker from './serviceWorker';
-import Controls from './components/Controls';
-import Scene from './components/Scene';
-import './index.css';
+import React, { useRef, useState, Fragment } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
 
-function App() {
+// import './styles.css';
+
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => {
+    if (hovered && !active) {
+      mesh.current.rotation.z += 0.01;
+      mesh.current.rotation.x += 0.01;
+    }
+    if (hovered && active) {
+      mesh.current.rotation.y += 0.02;
+      mesh.current.rotation.x += 0.06;
+    }
+  });
+
   return (
-    <Canvas camera={{ zoom: 40, position: [0, 0, 500] }}>
-      <Suspense
-        fallback={<Dom center className="loading" children="Loading..." />}
-      >
-        {' '}
-        <Controls />
-        <Scene />
-      </Suspense>
-    </Canvas>
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [6, 6, 6] : [1, 1, 1]}
+      onClick={(e) => setActive(!active)}
+      onPointerOver={(e) => setHover(true)}
+      onPointerOut={(e) => setHover(false)}
+    >
+      <boxBufferGeometry attach="geometry" args={[3, 3, 3]} />
+      <meshStandardMaterial
+        attach="material"
+        color={hovered ? 'hotpink' : 'orange'}
+      />
+    </mesh>
   );
 }
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Fragment>
+    <h1>Hello, this is react-three-fiber!</h1>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-2.2, 0, 0]} />
+      <Box position={[2.2, 0, 0]} />
+    </Canvas>
+  </Fragment>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
