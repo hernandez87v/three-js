86caba1cd864c9831dd7a1a118b6525063f05252
hernandez87v <hernandez87v@hotmail.com>
2020-10-06 14:31:01 -0700

    add bounce back from paddle

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import { Physics, useSphere, useBox, usePlane } from 'use-cannon';
 // import './App.css';
 
 function Ball({ args = [0.5, 32, 32] }) {
-  const [ref] = useSphere(() => ({ args: 0.5, mass: 1 }))
+  const { viewport } = useThree()
+  const [ref, api] = useSphere(() => ({ args: 0.5, mass: 1 }))
+
+  usePlane(() => (
+    {
+      position: [0, -viewport.height, 0],
+      rotation: [-Math.PI / 2, 0, 0],
+      onCollide: () => {
+        api.position.set(0, 0, 0)
+        api.velocity.set(0, 10, 0)
+      }
+    })
+  )
   return (
     <mesh ref={ref}>
       <sphereBufferGeometry args={args} />
