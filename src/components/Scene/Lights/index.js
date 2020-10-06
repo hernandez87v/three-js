ab627ed67de1e4ba7b75f42f0d5ffe8d06afb036
hernandez87v <hernandez87v@hotmail.com>
2020-10-05 20:46:03 -0700

    add scene canvas and lights

---
+++ src/components/Scene/Lights/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default () => {
+  const FakeSphere = () => (
+    <mesh>
+      <sphereBufferGeometry attach="geometry" args={[0.7, 30, 30]} />
+      <meshBasicMaterial attach="material" color={0xfff1ef} />
+    </mesh>
+  );
+
+  return (
+    <group>
+      <FakeSphere />
+      <ambientLight position={[0, 4, 0]} intensity={0.3} />
+      <directionalLight intensity={0.5} position={[0, 0, 0]} color={0xffffff} />
+      <pointLight intensity={1.9} position={[-6, 3, -6]} color={0xffcc77} />
+      <pointLight intensity={1.9} position={[6, 3, 6]} color={0xffcc77} />
+    </group>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { Canvas, Dom } from 'react-three-fiber';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <Canvas camera={{ zoom: 40, position: [0, 0, 500] }}>
+      <Suspense
+        fallback={<Dom center className="loading" children="Loading..." />}
+      ></Suspense>
+    </Canvas>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
