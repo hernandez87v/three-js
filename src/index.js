b87471cc026df48aa2a18e11129f992332bcb22d
hernandez87v <hernandez87v@hotmail.com>
2020-10-05 21:13:43 -0700

    added rerrain and changed index/src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Canvas, Dom } from 'react-three-fiber';
+import * as serviceWorker from './serviceWorker';
+import Controls from './components/Controls';
+import Scene from './components/Scene';
 import './index.css';
 
 function App() {
@@ -8,10 +11,22 @@ function App() {
     <Canvas camera={{ zoom: 40, position: [0, 0, 500] }}>
       <Suspense
         fallback={<Dom center className="loading" children="Loading..." />}
-      ></Suspense>
+      >
+        {' '}
+        <Controls />
+        <Scene />
+      </Suspense>
     </Canvas>
   );
 }
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const root = document.getElementById('root');
-ReactDOM.render(<App />, root);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
