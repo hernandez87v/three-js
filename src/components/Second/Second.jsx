ee5f3447a56a4f14d2b23a6ed5b0f7a41ecf3c06
hernandez87v <hernandez87v@hotmail.com>
2020-10-09 14:17:52 -0700

    🕶firgured out why shadows werent showing now fixed

--- src/components/Second/Second.jsx
+++ src/components/Second/Second.jsx
@@ -34,13 +34,13 @@ export default function Second() {
           position={[0, 10, 0]}
           intensity={1.5}
           castShadow
-          shadow-mapSize-height={1024}
           shadow-mapSize-width={1024}
-          shadow-camera-far={500}
-          shadow-camera-left={10}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
           shadow-camera-right={10}
           shadow-camera-top={10}
-          shadow-camera-bottom={10}
+          shadow-camera-bottom={-10}
         />
         <pointLight position={[-10, 0, -20]} intensity={0.5} />
         <pointLight position={[0, -10, 0]} intensity={1.5} />
