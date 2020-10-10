d35b425ecf3befc5f082e07e60844b33c26e4e63
hernandez87v <hernandez87v@hotmail.com>
2020-10-09 17:50:37 -0700

    removed service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 import './index.css';
