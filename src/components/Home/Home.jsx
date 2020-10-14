0cd975a4f20276107e31826253d2f83190038fdd
hernandez87v <hernandez87v@hotmail.com>
2020-10-14 14:50:34 -0700

    📐adding scene

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,10 +10,32 @@ export default function Home() {
         <Canvas
           colorManagement
           shadowMap
-          camera={{ position: [45, 2, 0.1, 100], fov: 50 }}
+          camera={{ position: [45, 2, 50], fov: 10 }}
         >
+          <ambientLight intensity={0.3} />
+          <directionalLight
+            // position={[0, 10, 0]}
+            // intensity={1.5}
+            castShadow
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+          />
+          <group>
+            <mesh
+              receiveShadow
+              //   rotation={[-Math.PI / 2, 0, 0]}
+              position={[0, -5, 0]}
+            >
+              <shadowMaterial transparent opacity={0.5} />
+              <planeBufferGeometry
+                rotation={[-0.5 * Math.PI]}
+                attach="geometry"
+                args={[10, 10]}
+              />
+              <Box attach="geometry" color="blue" />
+            </mesh>
+          </group>
           <OrbitControls />
-          <Box />
         </Canvas>
       </div>
     </>
