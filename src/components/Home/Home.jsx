534fc5685a8191b3a16c76615c73502bdd1ee3ba
hernandez87v <hernandez87v@hotmail.com>
2020-10-14 17:03:31 -0700

    🎥 camera perspective not quite right but saving for now to work on later

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,54 +2,36 @@ import React from 'react';
 import { Box, OrbitControls } from 'drei';
 import { Canvas } from 'react-three-fiber';
 // import * as THREE from 'three';
+// import './Home.css';
 
 export default function Home() {
   return (
     <>
-      <div>
-        <Canvas
-          //   colorManagement
-          shadowMap
-          camera={{ position: [0, 5, 1] }}
-          //   camera={{ position: [45, 2, 0.1, 100] }}
-        >
-          <perspectiveCamera
-            // fov={45}
-            // aspect={2}
-            // near={0.1}
-            // far={100}
-            // position={[45, 2, 0.1, 100]}
-            aspect
-            // camera={(0, 5, 1)}
-            // camera={{
-            //   fov: [45],
-            //   aspect: [2],
-            //   near: [0.1],
-            //   far: [100],
-            // }}
+      <div className="home-grid">
+        <Canvas colorManagement shadowMap perspectiveCamera={[45, 2, 0.1, 100]}>
+          {/* <perspectiveCamera
+            args={[45, 2, 0.1, 100]}
             updateProjectionMatrix
-          >
-            <ambientLight intensity={0.3} />
-            <directionalLight
-              castShadow={true}
-              shadow-mapSize-width={1024}
-              shadow-mapSize-height={1024}
-            />
-            <OrbitControls autoRotate />
-
-            <group>
-              <mesh receiveShadow>
-                <planeBufferGeometry
-                  receiveShadw
-                  rotateX={[-0.5 * Math.PI]}
-                  attach="geometry"
-                  args={[10, 10]}
-                  //   shadowMaterial={{ transparent: true, opacity: [0.5] }}
-                />
-                <Box attach="material" color="blue" />
-              </mesh>
-            </group>
-          </perspectiveCamera>
+          > */}
+          <ambientLight intensity={0.3} />
+          <directionalLight
+            castShadow
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+          />
+          <OrbitControls autoRotate />
+          <group>
+            <mesh receiveShadow rotateX={[-0.5 * Math.PI]} position={[0, 5, 1]}>
+              <planeBufferGeometry
+                receiveShadw
+                attach="geometry"
+                args={[10, 10]}
+                //   shadowMaterial={{ transparent: true, opacity: [0.5] }}
+              />
+              <gridHelper />
+              <Box attach="material" color="blue" />
+            </mesh>
+          </group>
         </Canvas>
       </div>
     </>
