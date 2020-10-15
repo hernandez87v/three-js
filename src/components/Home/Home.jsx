7eebf22955cbc0770eccdb88b0081ee52a4dcbb0
hernandez87v <hernandez87v@hotmail.com>
2020-10-15 11:54:56 -0700

    📦 add box geometry

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,16 +3,28 @@ import { Box, OrbitControls } from 'drei';
 import { Canvas } from 'react-three-fiber';
 // import * as THREE from 'three';
 // import './Home.css';
+import { BackSide } from 'three';
 
 export default function Home() {
+  const BoxGlow = () => {
+    return (
+      <mesh>
+        <boxBufferGeometry attach="geometry" />
+        <meshBasicMaterial color={0xfff1ef} attach="material" />
+      </mesh>
+    );
+  };
+
   return (
     <>
       <div className="home-grid">
-        <Canvas colorManagement shadowMap perspectiveCamera={[45, 2, 0.1, 100]}>
-          {/* <perspectiveCamera
-            args={[45, 2, 0.1, 100]}
-            updateProjectionMatrix
-          > */}
+        <Canvas
+          // camera={{
+          //   position: [0, 5, 1],
+          // }}
+          colorManagement
+          shadowMap
+        >
           <ambientLight intensity={0.3} />
           <directionalLight
             castShadow
@@ -21,15 +33,25 @@ export default function Home() {
           />
           <OrbitControls autoRotate />
           <group>
-            <mesh receiveShadow rotateX={[-0.5 * Math.PI]} position={[0, 5, 1]}>
-              <planeBufferGeometry
-                receiveShadw
-                attach="geometry"
-                args={[10, 10]}
-                //   shadowMaterial={{ transparent: true, opacity: [0.5] }}
-              />
-              <gridHelper />
-              <Box attach="material" color="blue" />
+            <mesh receiveShadow position={[0, -5, 1]}>
+              <perspectiveCamera fov={45} aspect={2} near={0.1} far={100}>
+                <planeBufferGeometry
+                  receiveShadw
+                  attach="geometry"
+                  args={[10, 10]}
+
+                  // rotateX={-0.5 * Math.PI}
+                  //   shadowMaterial={{ transparent: true, opacity: [0.5] }}
+                />
+                <meshStandardMaterial
+                  color={0xd2452b}
+                  attach="material"
+                  side={BackSide}
+                  metalness={0.4}
+                />
+                <BoxGlow />
+                <gridHelper />
+              </perspectiveCamera>
             </mesh>
           </group>
         </Canvas>
