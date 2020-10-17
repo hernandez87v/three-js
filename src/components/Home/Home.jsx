c601fed641a227406b76e98cd0f2f44f3a0ab0bb
hernandez87v <hernandez87v@hotmail.com>
2020-10-17 14:35:36 -0700

    🔍fix emoji hmtl for better accesibility

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,14 @@ export default function Home() {
   return (
     <div className="home-bg">
       <div className="page-title">
-        <h1 className="neumorph">🖱 ☝</h1>
+        <h1 className="neumorph">
+          <span role="img" aria-label="computer muse">
+            🖱
+          </span>
+          <span role="img" aria-label="index pointing up">
+            ☝
+          </span>
+        </h1>
       </div>
     </div>
   );
