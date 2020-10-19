c88bdc29b5c194f0ce03525ff26b9c2d85d3ba3c
hernandez87v <hernandez87v@hotmail.com>
2020-10-18 17:00:46 -0700

    🔠 add text anitmation to home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,59 @@
-import React from 'react';
-import './Home.css';
+// import * as THREE from 'three';
+import React, { Suspense, useRef } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import Text from './Text';
+
+function Jumbo() {
+  const ref = useRef();
+  useFrame(
+    ({ clock }) =>
+      (ref.current.rotation.x = ref.current.rotation.y = ref.current.rotation.z =
+        Math.sin(clock.getElapsedTime()) * 0.3)
+  );
+  return (
+    <group ref={ref}>
+      <Text hAlign="left" position={[0, 4.2, 0]} children="REACT" />
+      <Text hAlign="left" position={[0, 0, 0]} children="THREE" />
+      <Text hAlign="left" position={[0, -4.2, 0]} children="FIBER" />
+      <Text hAlign="left" position={[12, 0, 0]} children="4" size={3} />
+      <Text hAlign="left" position={[16.5, -4.2, 0]} children="X" />
+    </group>
+  );
+}
+
+// let i = 0;
+
+// This component was auto-generated from GLTF by: https://github.com/react-spring/gltfjsx
 
 export default function Home() {
   return (
-    <div className="home-bg">
-      <div className="page-title">
-        <h1 className="neumorph">
-          <span role="img" aria-label="computer muse">
-            🖱
-          </span>
-          <span role="img" aria-label="index pointing up">
-            ☝
-          </span>
-        </h1>
-      </div>
-    </div>
+    <Canvas camera={{ position: [0, 0, 35] }}>
+      <ambientLight intensity={2} />
+      <pointLight position={[40, 40, 40]} />
+      <Suspense fallback={null}>
+        <Jumbo />
+      </Suspense>
+    </Canvas>
   );
 }
+
+// import React from 'react';
+// import './Home.css';
+
+// export default function Home() {
+//   return (
+//     <div className="home-bg">
+//       <div className="page-title">
+//         <h1 className="neumorph">
+//           <span role="img" aria-label="computer muse">
+//             🖱
+//           </span>
+//           <span role="img" aria-label="index pointing up">
+//             ☝
+//           </span>
+//         </h1>
+//       </div>
+//     </div>
+//   );
+// }
