811e3d866ab3c018677c03fc74bb996541fd286c
hernandez87v <hernandez87v@hotmail.com>
2020-11-09 11:09:27 -0800

    try to get ☀ and 🌑 to show in scene

--- src/components/SunAndMoon/SunAndMoon.jsx
+++ src/components/SunAndMoon/SunAndMoon.jsx
@@ -1,31 +1,28 @@
 import { OrbitControls, Stars } from 'drei';
 import React, { Suspense, lazy } from 'react';
 import { Canvas } from 'react-three-fiber';
+import './SunAndMoon.css';
 
 const Welcome = lazy(() => import('./Welcome'));
-// const SunMoon = lazy(() => import('./SunMoon/SunMoon'));
 
-export default function Home() {
+export default function SunAndMoon() {
   return (
-    <div className="Home">
-          <Suspense fallback={<div>Loading...</div>}>
-            <Canvas
-              colorManagement
-              camera={{ position: [100, 30, 100], fov: 100 }}
-            >
-              <OrbitControls
-                enableZoom={false}
-                enabled={false}
-                autoRotate
-                autoRotateSpeed={0.6}
-              />
-              <Stars radius={75} count={5000} />
-            </Canvas>
-          </Suspense>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Welcome />
-            {/* <SunMoon /> */}
-          </Suspense>
+    <div className="SunAndMoon">
+      <Canvas
+        colorManagement
+        camera={{ position: [100, 30, 100], fov: 100 }}
+      >
+        <OrbitControls
+          enableZoom={false}
+          enabled={false}
+          autoRotate
+          autoRotateSpeed={0.6}
+        />
+        <Stars radius={75} count={5000} />
+      </Canvas>
+      <Suspense fallback={null}>
+        <Welcome />
+      </Suspense>
     </div>
   );
 }
